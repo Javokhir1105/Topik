7654bd997568950f3f8f5983731cbd4fdb7a9f9d
Javokhir1105 <161455530+Javokhir1105@users.noreply.github.com>
2024-08-30 18:38:59 +0500

    width o'zgartirildi

--- src/Homepage/Content/Content.jsx
+++ src/Homepage/Content/Content.jsx
@@ -26,7 +26,7 @@ function Content() {
                         <button className="content-btn mt-6 p-8 px-20 border-2 rounded-full border-green-700 hover:bg-emerald-700 active:bg-blue-700 bg-emerald-400 text-white text-xl">{t('content.content-title6')}</button>
                         <p className="content-text-last mt-6 ml-8 text-xl">{t('content.content-title5')}</p>
                     </ul>
-                    <img src={rasm} alt="" className="content-img absolute  right-14 top-0 max-w-5xl max-xl:max-w-3xl buttom-0 top-16 z-0 max-lg:invisible" />
+                    <img src={rasm} alt="" className="content-img absolute  right-8 buttom-4 max-w-4xl max-xl:max-w-3xl buttom-0 top-16 z-0 max-lg:invisible" />
                 </div>
             </div>
         </div>
